5fd928da37c7b19ae49bb118b4586c0d4e8cb949
Andrey Radionov <andyomsk@gmail.com>
2020-05-02 22:10:07 +0600

    Second ViewController added

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,14 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stopRecordingButton.isEnabled = false
+    }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("Record button was pressed")
+        recordingLabel.text = "Recoring in process"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
     
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "Tap to record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+    }
 }
 
